30f07850b0181c5a1a0fe4cf59f5251eefa197ad
Carter Albrecht <cartera@knights.ucf.edu>
2020-09-11 18:51:52 -0400

    add search endpoint

---
+++ api/search.php
@@ -0,0 +1,66 @@
+<?php
+
+	$inData = getRequestInfo();
+
+  include_once 'config.php';
+
+	if ($conn->connect_error)
+	{
+		returnWithError($conn->connect_error);
+	}
+	else
+	{
+    $userQuery = $inData["userQuery"];
+    $userID = $inData["userID"];
+
+    $sql =
+      "SELECT * FROM contacts WHERE user_id={$userID}
+      AND (firstname LIKE '%{$userQuery}' OR lastname LIKE '%{$userQuery}' OR phone LIKE '%{$userQuery}' OR email LIKE '%{$userQuery}')";
+
+		$result = $conn->query($sql);
+
+    if ($result->num_rows > 0)
+    {
+      $searchResults = array();
+      while ($row = $result->fetch_assoc())
+      {
+          $contact = array('firstname' => $row['firstname'],
+                           'lastname'  => $row['lastname'],
+                           'phone'     => $row['phone'],
+                           'email'     => $row['email']);
+
+          array_push($searchResults, $contact);
+      }
+      returnWithInfo($searchResults);
+    }
+    else
+    {
+      returnWithError("No Records Found");
+    }
+    $conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+  function returnWithError($err)
+	{
+    $retValue = '{"results":,"error":"' . $err .'"}';
+		sendResultInfoAsJson($retValue);
+	}
+
+	function returnWithInfo($searchResults)
+	{
+    $retValue = '{"results":'. json_encode($searchResults) . ',"error":""}';
+		sendResultInfoAsJson($retValue);
+	}
+
+?>
